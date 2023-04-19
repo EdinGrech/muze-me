e28bb12e0231448811d91100b32100cb62ba25d8
EdinGrech <edin.grech@gmail.com>
2023-04-19 10:38:29 +0200

    user api tweeks

--- server/users/views.py
+++ server/users/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from .models import User
 from .serializers import UserSerializer, UserLoginSerializer, UserLogoutSerializer
-
+from rest_framework.permissions import IsAuthenticated
 
 class Register(generics.ListCreateAPIView):
     # get method handler
@@ -13,16 +13,20 @@ class Register(generics.ListCreateAPIView):
 
 
 class Login(generics.GenericAPIView):
-    # get method handler
     queryset = User.objects.all()
     serializer_class = UserLoginSerializer
 
     def post(self, request, *args, **kwargs):
         serializer_class = UserLoginSerializer(data=request.data)
         if serializer_class.is_valid(raise_exception=True):
-            return Response(serializer_class.data, status=HTTP_200_OK)
+            return Response(
+                # return the token and email
+                data={
+                    'user_id': serializer_class.data['user_id'],
+                    'token': serializer_class.data['token'],
+                    },
+                status=HTTP_200_OK)
         return Response(serializer_class.errors, status=HTTP_400_BAD_REQUEST)
-
 
 class Logout(generics.GenericAPIView):
     queryset = User.objects.all()
@@ -33,6 +37,24 @@ class Logout(generics.GenericAPIView):
         if serializer_class.is_valid(raise_exception=True):
             return Response(serializer_class.data, status=HTTP_200_OK)
         return Response(serializer_class.errors, status=HTTP_400_BAD_REQUEST)
+
+class Profile(generics.RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        return Response(UserSerializer(request.user).data)
+    
+    def update(self, request, *args, **kwargs):
+        # only update user news_tollerance, make sure it is an integer between 0 - 10
+        if request.data['news_tollerance'] >= 0 and request.data['news_tollerance'] <= 10:
+            request.user.news_tollerance = request.data['news_tollerance']
+            request.user.save()
+            return Response(UserSerializer(request.user).data)
+        else:
+            return Response("news_tollerance must be an integer between 0 - 10")
+        
 
 
 
