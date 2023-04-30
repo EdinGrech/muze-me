6b163835f3c3e00f7c02104b1bf2a49887f602f3
Ed <edin.grech@gmail.com>
2023-05-01 00:18:13 +0200

    Functional auth, start of infanate scroll

--- client/muze-me/src/app/home/home.page.ts
+++ client/muze-me/src/app/home/home.page.ts
@@ -3,19 +3,33 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { SideBarComponent } from '../components/side-bar/side-bar.component';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
+import { News } from '../interfaces/news';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, SideBarComponent]
+  imports: [IonicModule, CommonModule, FormsModule, SideBarComponent],
 })
 export class HomePage implements OnInit {
-
-  constructor() { }
+  constructor() {}
+  newsList:News[] = [];
 
   ngOnInit() {
+    this.generateItems();
   }
 
+  private generateItems() {
+    const count = (this.newsList.length / 10);
+    
+  }
+
+  onIonInfinite(ev: Event) {
+    this.generateItems();
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
+  }
 }
