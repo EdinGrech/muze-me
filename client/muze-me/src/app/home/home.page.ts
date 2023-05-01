72c9a24c0e2ce6718ecac4bea26be9dea0b8c747
Ed <edin.grech@gmail.com>
2023-05-01 21:18:13 +0200

    fab scroll to top

--- client/muze-me/src/app/home/home.page.ts
+++ client/muze-me/src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
@@ -11,48 +11,44 @@ import { Observable } from 'rxjs';
 import { loadNews } from '../state/news/news.actions';
 
 import { NewsCardComponent } from './components/news-card/news-card.component';
-import { loadUser } from '../state/user/user.actions';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    SideBarComponent,
-    NewsCardComponent,
-  ],
+  imports: [IonicModule, CommonModule, FormsModule, SideBarComponent, NewsCardComponent],
 })
 export class HomePage implements OnInit {
-  constructor(private store: Store<{ auth: any; news: any }>) {}
-  newsList$: News[] = [];
-  newNewsList$: Observable<News[]> = this.store.select(
-    (state) => state.news.news
-  );
-  error$: any = this.store.select((state) => state.news.error);
-  loading$: any = this.store.select((state) => state.news.loading);
-  user$: any = this.store.select((state) => state.auth.user);
+  constructor(
+    private store: Store<{auth:any, news: any}>,
+    ) {
+    this.newNewsList$.subscribe((news:News[]) => {
+      console.log(news);
+      this.newsList$ = this.newsList$.concat(news);
+      console.log(this.newsList$);
+    });
+  }
+  newsList$:News[] = [];
+  newNewsList$:Observable<News[]> = this.store.select(state => state.news.news);
+  error$:any = this.store.select(state => state.news.error);
+  loading$:any = this.store.select(state => state.news.loading);
+  user$:any = this.store.select(state => state.auth.user);
 
   ngOnInit() {
-    this.store.dispatch(loadUser());
     this.generateItems();
   }
 
   private generateItems() {
-    const page = this.newsList$.length / 10 + 1;
-    this.user$.subscribe((user: any) => {
-      let tollerance: number;
-      if (user) {
+    const page = (this.newsList$.length / 10)+1;
+    let tollerance: number;
+    this.user$.subscribe((user:any) => {
+      if(user){
         tollerance = user.news_tollerance;
       }
-      this.store.dispatch(loadNews({ page: page, tollerance: tollerance! })); //to get tollerence from  user at some point
-      this.newNewsList$.subscribe((news: News[]) => {
-        this.newsList$ = this.newsList$.concat(news);
-      });
     });
+    this.store.dispatch(loadNews({ page: page, tollerance: tollerance! }));//to get tollerence from  user at some point   
   }
 
   onIonInfinite(ev: Event) {
@@ -60,5 +56,22 @@ export class HomePage implements OnInit {
     setTimeout(() => {
       (ev as InfiniteScrollCustomEvent).target.complete();
     }, 500);
+  }
+
+  lastScrollSpot: number = 0;
+  onScroll(event: any) {
+    const scrollPosition = event.detail.scrollTop;
+    const fab = document.querySelector('ion-fab');
+    if(scrollPosition < this.lastScrollSpot){  
+      fab?.classList.add('show');
+    } else {
+      fab?.classList.remove('show');
+    }
+    this.lastScrollSpot = scrollPosition;
+  }
+  
+  @ViewChild(IonContent) content!: IonContent;
+  scrollToTop() {
+    this.content.scrollToTop(this.newsList$.length*20);
   }
 }
